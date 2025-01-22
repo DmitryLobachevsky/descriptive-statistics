8ee5549f05b181c1f035c105aa03cce92b824def
lobachevskiy <dimkagad99@gmail.com>
2025-01-22 10:23:10 +0300

    Add median() methods

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ def random_list(n):
 def mean(lst):
     return sum(lst)/len(lst) if lst else float('nan')
 
+def median(lst):
+    len_lst = len(lst)
+    return lst[len_lst // 2] if len_lst % 2 != 0 else ((lst[len_lst // 2 - 1]+ lst[len_lst // 2])/2)
+
 n = int(input('Enter the array dimension: '))
 
 lst = random_list(n)
 
 print(lst)
 print('Mean value: ', mean(lst))
+print('Median: ', median(lst))
